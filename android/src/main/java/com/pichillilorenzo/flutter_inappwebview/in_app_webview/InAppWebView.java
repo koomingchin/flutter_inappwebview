be7716c5871a13548be5563fb3bd8a2e99313386
koolong <koomingchin@gmail.com>
2022-08-08 11:31:31 +0800

    Update InAppWebView.java

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/in_app_webview/InAppWebView.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/in_app_webview/InAppWebView.java
@@ -577,7 +577,7 @@ final public class InAppWebView extends InputAwareWebView implements InAppWebVie
       @Override
       public void run() {
         try {
-          Bitmap screenshotBitmap = Bitmap.createBitmap(getMeasuredWidth(), 10, Bitmap.Config.ARGB_8888);
+          Bitmap screenshotBitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
           Canvas c = new Canvas(screenshotBitmap);
           c.translate(-getScrollX(), -getScrollY());
           draw(c);
